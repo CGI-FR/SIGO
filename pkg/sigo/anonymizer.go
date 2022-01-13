f17d6d093da8777b84b9942078d1606ac76b22ac
Marie Giraud <marie.giraud@cgi.com>
2022-01-13 16:45:28 +0000

    feat: add aggregation anonymizer

--- pkg/sigo/anonymizer.go
+++ pkg/sigo/anonymizer.go
@@ -1,17 +1,28 @@
 package sigo
 
 import (
+	"encoding/json"
+	"math"
 	"math/rand"
+	"sort"
 )
 
 func NewNoAnonymizer() NoAnonymizer { return NoAnonymizer{} }
 
-type NoAnonymizer struct{}
-
-type AnonymizedRecord struct {
-	original Record
-	mask     map[string]interface{}
+func NewAggregationAnonymizer(typeAgg string) AggregationAnonymizer {
+	return AggregationAnonymizer{typeAggregation: typeAgg}
 }
+
+type (
+	NoAnonymizer          struct{}
+	AggregationAnonymizer struct {
+		typeAggregation string
+	}
+	AnonymizedRecord struct {
+		original Record
+		mask     map[string]interface{}
+	}
+)
 
 func (ar AnonymizedRecord) QuasiIdentifer() []float32 {
 	return ar.original.QuasiIdentifer()
@@ -47,4 +58,57 @@ func (a NoAnonymizer) Anonymize(rec Record, clus Cluster) Record {
 	mask["y"] = choice.Row()["y"]
 
 	return AnonymizedRecord{original: rec, mask: mask}
+}
+
+func (a AggregationAnonymizer) Anonymize(rec Record, clus Cluster) Record {
+	values := make(map[string][]float64)
+
+	for _, record := range clus.Records() {
+		for key, value := range record.Row() {
+			switch v := value.(type) {
+			case json.Number:
+				val, _ := v.Float64()
+				values[key] = append(values[key], val)
+			case int:
+				values[key] = append(values[key], float64(v))
+			default:
+				continue
+			}
+		}
+	}
+
+	mask := map[string]interface{}{}
+
+	for key := range values {
+		switch a.typeAggregation {
+		case "mean":
+			mask[key] = mean(values[key])
+		case "median":
+			mask[key] = median(values[key])
+		}
+	}
+
+	return AnonymizedRecord{original: rec, mask: mask}
+}
+
+func mean(listValues []float64) (m float64) {
+	for _, val := range listValues {
+		m += val
+	}
+
+	m /= float64(len(listValues))
+	//nolint: gomnd
+	return math.Round(m*100) / 100
+}
+
+func median(listValues []float64) (m float64) {
+	sort.Float64s(listValues)
+	lenList := len(listValues)
+
+	if lenList%2 == 0 {
+		//nolint: gomnd
+		return (listValues[lenList/2] + listValues[lenList/2-1]) / 2
+	}
+
+	return listValues[(lenList-1)/2]
 }
